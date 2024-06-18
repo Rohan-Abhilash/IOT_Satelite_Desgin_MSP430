1c7cdfd4b3b5933117df7d40140d2ab29ee5b1b0
Rohan-Abhilash <120775348+Rohan-Abhilash@users.noreply.github.com>
2024-06-18 15:33:18 +0530

    added all the input and output pins information

--- main.c
+++ main.c
@@ -8,6 +8,18 @@
 #define PIZERO_EN BIT4 //GPIO1 p1.4 5V to PI zero switch
 #define BURN_EN BIT0 //P3.0 for enabling the burn wire
 #define BAT_VOLT BIT6 //P1.6 for receiving the voltage of the battery
+#define SKYSATCOM_UART_TX BIT3 //4.3 used for transmitting data to the skysatcom
+#define SKYSATCOM_UART_RX BIT2 //4.2 used for receiving data from the skysatcom
+#define PIZERO_MOSI BIT0 //2.0 used for getting input from the pizero
+#define PIZERO_MISO BIT1 //2.1 used for sending output to the pizero
+#define LORA_MISO BIT5 //3.5 used for getting data from Lora
+#define LORA_MOSI BIT6 //3.4 used for sending the data to the Lora board
+#define LORA_CS BIT7 //3.7 used for choosing the mode of operation
+#define LORA_SCK BIT6 //3.6 used for selecting the clock used for the communication
+#define PIZERO_CS BIT3 //2.3 used for selecting the mode of operation
+#define PIZERO_SCK BIT2 //2.2 used for selecting the clock for communication
+#define LORA_G0 BIT4 // 2.4 used for incoming interrupt requests from the LORA board
+#define LORA_RST BIT5 //2.5 used for reseting the LORA board in case of any glitches or hangs
 
 void configureADC(void);
 void configurePins(void);
@@ -33,22 +45,21 @@ void main(void)
 
         switch_control(adc_value);
 
-        __delay_cycles(100000); // Delay for demonstration purposes
+        __delay_cycles(100000);
     }
 }
 
 void configureADC(void)
 {
-    // Configure P1.0 for ADC
     P1SEL0 |= BIT6;
     P1SEL1 |= BIT6;
 
-    // Configure ADC12_B
     ADC12CTL0 &= ~ADC12ENC;              // Disable ADC12_B before configuration
     ADC12CTL0 = ADC12SHT0_2 | ADC12ON;   // Sampling time, ADC12 on
+
     ADC12CTL1 = ADC12SHP;                // Use sampling timer
     ADC12CTL2 = ADC12RES_2;              // 12-bit conversion results
-    ADC12MCTL0 = ADC12INCH_0;            // A0 ADC input select; Vref=AVCC
+    ADC12MCTL0 = ADC12INCH_0;
     ADC12IER0 = 0;                       // Disable ADC interrupts
     ADC12CTL0 |= ADC12ENC;               // Enable ADC12_B
 }
@@ -62,7 +73,7 @@ void configurePins(void){
     P1OUT &= ~PIZERO_EN;
     P4OUT &= ~LORA_EN;
     P5OUT &= ~SKYSAT_EN;
-    P3OUT &= ~BURN_EN; // setting the burn wire to low till antenna has to be deployed
+    P3OUT &= ~BURN_EN;  // setting the burn wire to low till antenna has to be deployed
 
 }
 void enable_burn_wire(void){
@@ -73,7 +84,7 @@ void enable_burn_wire(void){
 
 unsigned int readADC(void)
 {
-    ADC12CTL0 |= ADC12SC;                // Start sampling/conversion
+    ADC12CTL0 |= ADC12SC;                // Start sampling
     while (ADC12CTL1 & ADC12BUSY);       // Wait for conversion to complete
     return ADC12MEM0;                    // Return ADC value
 }
