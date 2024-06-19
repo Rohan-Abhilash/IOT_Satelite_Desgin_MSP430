487968dd28bc243fc83557e80f262d44326bcc2d
Rohan-Abhilash <120775348+Rohan-Abhilash@users.noreply.github.com>
2024-06-19 11:35:33 +0530

    added functions initializing lora modules and spi modules SPI_send SPI_receive

--- main.c
+++ main.c
@@ -1,27 +1,32 @@
 #include <msp430.h>
+#include <stdint.h>
 
 #define REF_VOLTAGE 3.3
 #define THRESHOLD_50 (4095 * 0.5)  // 50% of 3.3V
 #define THRESHOLD_30 (4095 * 0.3)  // 30% of 3.3V
-#define LORA_EN BIT0 //P4.0 pin for enabling the Lora
-#define SKYSAT_EN BIT3 //GPIO4 p5.3 5v to SkySat switch
-#define PIZERO_EN BIT4 //GPIO1 p1.4 5V to PI zero switch
+
 #define BURN_EN BIT0 //P3.0 for enabling the burn wire
 #define BAT_VOLT BIT6 //P1.6 for receiving the voltage of the battery
+
+#define SKYSAT_EN BIT3 //GPIO4 p5.3 5v to SkySat switch
 #define SKYSATCOM_UART_TX BIT3 //4.3 used for transmitting data to the skysatcom
 #define SKYSATCOM_UART_RX BIT2 //4.2 used for receiving data from the skysatcom
+
+#define PIZERO_EN BIT4 //GPIO1 p1.4 5V to PI zero switch
 #define PIZERO_MOSI BIT0 //2.0 used for getting input from the pizero
 #define PIZERO_MISO BIT1 //2.1 used for sending output to the pizero
+#define PIZERO_CS BIT3 //2.3 used for selecting the mode of operation
+#define PIZERO_SCK BIT2 //2.2 used for selecting the clock for communication
+#define WATCHDOG_SIGNAL_PIN BIT6 // P2.6 changed as pin number 5.0 cannot have interrupts enabled
+#define RESET_SIGNAL_PIN    BIT7 // P2.7
+
+#define LORA_EN BIT0 //P4.0 pin for enabling the Lora
+#define LORA_G0 BIT4 // 2.4 used for incoming interrupt requests from the LORA board
+#define LORA_RST BIT5 //2.5 used for reseting the LORA board in case of any glitches or hangs
 #define LORA_MISO BIT5 //3.5 used for getting data from Lora
 #define LORA_MOSI BIT6 //3.4 used for sending the data to the Lora board
 #define LORA_CS BIT7 //3.7 used for choosing the mode of operation
 #define LORA_SCK BIT6 //3.6 used for selecting the clock used for the communication
-#define PIZERO_CS BIT3 //2.3 used for selecting the mode of operation
-#define PIZERO_SCK BIT2 //2.2 used for selecting the clock for communication
-#define LORA_G0 BIT4 // 2.4 used for incoming interrupt requests from the LORA board
-#define LORA_RST BIT5 //2.5 used for reseting the LORA board in case of any glitches or hangs
-#define WATCHDOG_SIGNAL_PIN BIT6 // P2.6 changed as pin number 5.0 cannot have interrupts enabled
-#define RESET_SIGNAL_PIN    BIT7 // P2.7
 
 void configureADC(void);
 void configurePins(void);
@@ -30,6 +35,8 @@ void sendResetSignal(void);
 void enable_burn_wire(void);
 unsigned int readADC(void);
 void switch_control(unsigned int);
+void SPI_send(uint8_t);
+uint8_t SPI_receive(void);
 
 void main(void)
 {
@@ -41,6 +48,9 @@ void main(void)
     configureADC();
 
     enable_burn_wire(); //enables the burn wire to deploy antennas for communication
+
+    SPI_init();
+    Lora_init();
 
     _BIS_SR(GIE);
 
@@ -96,6 +106,57 @@ void configurePins(void){
     P2IES &= ~WATCHDOG_SIGNAL_PIN;  // Rising edge
     P2IFG &= ~WATCHDOG_SIGNAL_PIN;  // Clear interrupt flag
 
+}
+
+void SPI_init(void){
+    P2DIR |= LORA_G0 | LORA_RST;
+    P3DIR |= LORA_CS ;
+    P4DIR |= LORA_EN;
+
+    P2OUT &= ~LORA_RST;
+    P3OUT |= LORA_CS;
+    P4OUT &= ~LORA_EN;
+
+    // configuring the SPI communication ports
+    UCB0CTLW0 |= UCSWRST;      // Put eUSCI state machine in reset
+    UCB0CTLW0 |= UCMST | UCSYNC | UCCKPL | UCMSB; // 3-pin, 8-bit SPI master
+    UCB0CTLW0 |= UCSSEL__SMCLK; // SMCLK
+    UCB0BRW = 0x02;             // /2 clock divider
+    UCB0CTLW0 &= ~UCSWRST;      // Initialize eUSCI state machine
+    UCB0IE |= UCRXIE;
+
+    P2SEL0 |= LORA_MOSI | LORA_MISO;
+    P3SEL0 |= LORA_SCK;
+
+}
+
+void Lora_init(void){
+    Lora_reset();
+    Lora_enable();
+}
+
+void Lora_reset(void){
+    P2OUT &= ~LORA_RST; //active low reset
+    __delay_cycles(10000);
+    P2OUT |= LORA_RST;  //setting it back to its inactive position
+}
+
+void Lora_enable(void){
+    P4OUT |= LORA_EN;
+}
+
+void SPI_send(uint8_t data){
+    while(!(UCB0IFG & UCTXIFG));
+    UCB0TXBUF = data;
+    while(!(UCB0IFG & UCRXIFG));
+    (void)UCB0RXBUF;
+}
+
+uint8_t SPI_receive(void){
+    while(!(UCB0IFG & UCRXIFG));
+    UCB0TXBUF = 0x00;   //dummy read t0 generate clock signal
+    while(!(UCB0IFG & UCRXIFG));
+    return UCB0RXBUF;
 }
 
 void configureTimer(void)
