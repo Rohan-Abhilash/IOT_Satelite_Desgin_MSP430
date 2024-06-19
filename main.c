9410461f60bc0c26b83e7027c09b2d6d16c58ece
Rohan-Abhilash <120775348+Rohan-Abhilash@users.noreply.github.com>
2024-06-19 14:18:03 +0530

    corrected the data receiveing and sensing from i2c

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <msp430.h>
+#include <stdint.h>
 
 #define REF_VOLTAGE 3.3
-#define THRESHOLD_50 (4095 * 0.5)  // 50% of 3.3V
-#define THRESHOLD_30 (4095 * 0.3)  // 30% of 3.3V
+#define THRESHOLD_50 (3.3 * 0.5)  // 50% of 3.3V
+#define THRESHOLD_30 (3.3 * 0.3)  // 30% of 3.3V
+
+#define MAX17048G_ADDR 0x36
+
 #define LORA_EN BIT0 //P4.0 pin for enabling the Lora
 #define SKYSAT_EN BIT3 //GPIO4 p5.3 5v to SkySat switch
 #define PIZERO_EN BIT4 //GPIO1 p1.4 5V to PI zero switch
 #define BURN_EN BIT0 //P3.0 for enabling the burn wire
-#define BAT_VOLT BIT6 //P1.6 for receiving the voltage of the battery
+#define BAT_VOLT_SDA BIT6 //P1.6 for receiving the voltage of the battery
+#define BAT_VOLT_SCL BIT7 //p1.7 for the clokc signal which els when the data is ready
 #define SKYSATCOM_UART_TX BIT3 //4.3 used for transmitting data to the skysatcom
 #define SKYSATCOM_UART_RX BIT2 //4.2 used for receiving data from the skysatcom
 #define PIZERO_MOSI BIT0 //2.0 used for getting input from the pizero
@@ -23,52 +28,83 @@
 #define WATCHDOG_SIGNAL_PIN BIT6 // P2.6 changed as pin number 5.0 cannot have interrupts enabled
 #define RESET_SIGNAL_PIN    BIT7 // P2.7
 
-void configureADC(void);
 void configurePins(void);
 void configureTimer(void);
 void sendResetSignal(void);
 void enable_burn_wire(void);
-unsigned int readADC(void);
-void switch_control(unsigned int);
+void switch_control(float);
+void I2C_init(void);
+void I2C_Read(uint8_t , uint8_t* , uint8_t );
+
 
 void main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
     PM5CTL0 &= ~LOCKLPM5;
 
-    // Configure the 12 bit ADC
     configurePins();
-    configureADC();
 
     enable_burn_wire(); //enables the burn wire to deploy antennas for communication
+
+    uint8_t data[2];
+
+    I2C_init();
 
     _BIS_SR(GIE);
 
     // Main loop
     while(1)
     {
-        unsigned int adc_value = readADC();
+        I2C_Read(0x02,data,2);
 
-        switch_control(adc_value);
+        uint16_t voltage_raw = (data[1] << 8) | data[0];
+        float voltage = voltage_raw * 1.25 / 1000;
+
+        switch_control(voltage);
 
         __delay_cycles(100000);
     }
 }
 
-void configureADC(void)
-{
-    P1SEL0 |= BIT6;
-    P1SEL1 |= BIT6;
+void I2C_init(void) {
+    // Configure I2C pins
+    P1SEL0 |= BAT_VOLT_SDA | BAT_VOLT_SCL;      // P1.6 = SDA, P1.7 = SCL
+    P1SEL1 &= ~(BAT_VOLT_SDA| BAT_VOLT_SCL);
 
-    ADC12CTL0 &= ~ADC12ENC;              // Disable ADC12_B before configuration
-    ADC12CTL0 = ADC12SHT0_2 | ADC12ON;   // Sampling time, ADC12 on
-
-    ADC12CTL1 = ADC12SHP;                // Use sampling timer
-    ADC12CTL2 = ADC12RES_2;              // 12-bit conversion results
-    ADC12MCTL0 = ADC12INCH_0;
-    ADC12IER0 = 0;                       // Disable ADC interrupts
-    ADC12CTL0 |= ADC12ENC;               // Enable ADC12_B
+    // Configure USCI_B0 for I2C
+    UCB0CTLW0 |= UCSWRST;       // Put eUSCI_B in reset
+    UCB0CTLW0 |= UCMODE_3 | UCMST | UCSYNC; // I2C master mode, synchronous mode
+    UCB0CTLW0 |= UCSSEL__SMCLK; // SMCLK
+    UCB0BRW = 10;               // Set clock frequency to 100 kHz (assuming SMCLK is 1 MHz)
+    UCB0CTLW0 &= ~UCSWRST;      // Release eUSCI_B from reset
 }
+
+void I2C_Read(uint8_t reg, uint8_t *data, uint8_t length) {
+    // Set slave address
+    UCB0I2CSA = MAX17048G_ADDR;
+
+    // Generate start condition and send register address
+    UCB0CTLW0 |= UCTR + UCTXSTT; // I2C TX, start condition
+    while (UCB0CTLW0 & UCTXSTT); // Wait until start condition is sent
+    UCB0TXBUF = reg;             // Send register address
+    while (!(UCB0IFG & UCTXIFG0)); // Wait for TX buffer to be ready
+
+    // Generate repeated start condition and initiate read
+    UCB0CTLW0 &= ~UCTR;          // I2C RX
+    UCB0CTLW0 |= UCTXSTT;        // Repeated start condition
+    while (UCB0CTLW0 & UCTXSTT); // Wait until repeated start condition is sent
+
+    // Read data
+    int i = 0;
+    for (i = 0; i < length; i++) {
+        while (!(UCB0IFG & UCRXIFG0)); // Wait for RX buffer to be ready
+        data[i] = UCB0RXBUF;        // Read received data
+        if (i == length - 1) {
+            UCB0CTLW0 |= UCTXSTP;   // Generate stop condition
+        }
+    }
+}
+
 
 void configurePins(void){
     P1DIR |= PIZERO_EN;
@@ -119,22 +155,15 @@ void enable_burn_wire(void){
     P3OUT &= ~BURN_EN;
 }
 
-unsigned int readADC(void)
-{
-    ADC12CTL0 |= ADC12SC;                // Start sampling
-    while (ADC12CTL1 & ADC12BUSY);       // Wait for conversion to complete
-    return ADC12MEM0;                    // Return ADC value
-}
-
-void switch_control(unsigned int adc_value){
-    if(adc_value >= THRESHOLD_50)
+void switch_control(float voltage){
+    if(voltage >= THRESHOLD_50)
         {
             // If ADC value is >= 50% of 3.3V
             P1OUT |= PIZERO_EN;
             P4OUT |= LORA_EN;
             P5OUT |= SKYSAT_EN;
         }
-        else if(adc_value < THRESHOLD_30)
+        else if(voltage < THRESHOLD_30)
         {
             // If ADC value is < 30% of 3.3V
             P1OUT &= ~PIZERO_EN;
