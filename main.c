14c8dfd56ee21180cd75f73401c956d62ee455da
Rohan-Abhilash <120775348+Rohan-Abhilash@users.noreply.github.com>
2024-07-01 11:10:44 +0530

    all communication protocol working

--- main.c
+++ main.c
@@ -41,10 +41,11 @@
 #define LORA_EN BIT0 //P4.0 pin for enabling the Lora
 #define LORA_G0 BIT4 // 2.4 used for incoming interrupt requests from the LORA board
 #define LORA_RST BIT5 //2.5 used for reseting the LORA board in case of any glitches or hangs
+
+#define LORA_MOSI BIT4   //3.4 used for sending the data to the Lora board
 #define LORA_MISO BIT5 //3.5 used for getting data from Lora
-#define LORA_MOSI BIT6 //3.4 used for sending the data to the Lora board
-#define LORA_CS BIT7 //3.7 used for choosing the mode of operation
-#define LORA_SCK BIT6 //3.6 used for selecting the clock used for the communication
+#define LORA_SCK BIT6 //3.6 used for the clock of for the communication
+#define LORA_CS BIT7 //3.7 used for choosing the chip
 
 
 void configurePins(void);
@@ -57,68 +58,57 @@ void Lora_init(void);
 void Lora_reset(void);
 void Lora_enable(void);
 
-void VC_Sensor_configure_ina3221(void);
 void VC_Sensor_I2C_init(void);
-void VC_Sensor_I2C_write_word(uint8_t , uint16_t);
-uint16_t VC_Sensor_I2C_read_word(uint8_t);
+uint16_t VC_Sensor_I2C_read_word(void);
 float VC_Sensor_get_shunt_voltage(uint8_t);
 float VC_Sensor_get_bus_voltage(uint8_t);
 
 void pizero_spi_init(void);
 void pizero_spi_write(uint8_t);
-uint8_t pizero_spi_read(void);
-void pizero_spi_select(void);
-void pizero_spi_deselect(void);
+int pizero_spi_read(void);
 
-void SPI_init(void);
-void SPI_send(uint8_t);
-uint8_t SPI_receive(void);
+void Lora_spi_init(void);
+void Lora_spi_send(uint8_t);
+int Lora_spi_receive(void);
 
 void cell_I2C_init(void);
 float cell_I2C_read_data(void);
 
 
+int read_I2C_data_cell;
+uint16_t read_I2C_data_VC_sensor;
+uint8_t reg;
+
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
     PM5CTL0 &= ~LOCKLPM5;
-
     // Configure the 12 bit ADC
     configurePins();
 
     configureTimer();
 
-    cell_I2C_init();
-
-    VC_Sensor_I2C_init();
-    VC_Sensor_configure_ina3221(); // infinite wait here
-
-    pizero_spi_init();
-
     enable_burn_wire(); //enables the burn wire to deploy antennas for communication
 
-
-    _BIS_SR(GIE);
+    __enable_interrupt();
     // Main loop
     while(1)
     {
         //reading the value coming from the voltage current sensor
         uint8_t channel;
-
-
+        VC_Sensor_I2C_init();
         for(channel = 1;channel <=3 ; channel++){
             float shunt_voltage = VC_Sensor_get_shunt_voltage(channel); // shunt voltage of each channel
             float bus_voltage = VC_Sensor_get_bus_voltage(channel); //bus voltage of each channel
         }
-        float stateOfCharge = cell_I2C_read_data();
 
+        cell_I2C_init();
+        float stateOfCharge = cell_I2C_read_data();
         switch_control(stateOfCharge);
 
         //pizero communication module
-        pizero_spi_select();
+        pizero_spi_init();
         uint8_t pizero_data = pizero_spi_read();
-        pizero_spi_deselect();
-        //P1OUT &= ~BIT0;
 
         __delay_cycles(100000);
     }
@@ -128,147 +118,114 @@ int main(void)
 
 void pizero_spi_init(void) {
     // Set P2.3 as output (CS pin)
-    P2DIR |= BIT3;
-    P2OUT |= BIT3; // Set CS high
+    UCA0CTLW0 |= UCSWRST;
 
-    // Configure P2.0 (MOSI), P2.1 (MISO), and P2.2 (SCLK) for SPI
-    P2SEL0 |= BIT0 | BIT1 | BIT2;
-    P2SEL1 &= ~(BIT0 | BIT1 | BIT2);
+    UCA0CTLW0 |= UCMST;
+    UCA0CTLW0 |= UCMODE_2;  // mode 1 is for 4 pin-spi communication with active low CS bit
+    UCA0CTLW0 |= UCSSEL__SMCLK; //using the sub master clock
+    UCA0BRW    = 10;    // 100khz clock
+    UCA0CTLW0 |= UCSYNC;
+    UCA0CTLW0 |= UCSTEM;
+    UCA0CTLW0 |= UCMSB;
 
-    // Reset eUSCI_B0
-    UCB0CTLW0 = UCSWRST;
+    P2SEL0 |= PIZERO_MOSI | PIZERO_MISO | PIZERO_SCK | PIZERO_CS;
+    P2SEL1 &= ~(PIZERO_MOSI | PIZERO_MISO | PIZERO_SCK | PIZERO_CS);
 
-    // Configure eUSCI_B0 for SPI master mode
-    UCB0CTLW0 |= UCMST | UCSYNC | UCCKPH | UCMSB | UCSSEL_2; // 3-pin, 8-bit SPI master, clock phase, MSB first, SMCLK
-    UCB0BRW = 0x01; // Set clock prescaler
+    PM5CTL0 &= ~LOCKLPM5;
 
-    // Clear reset flag
-    UCB0CTLW0 &= ~UCSWRST;
+    UCA0CTLW0 &= ~UCSWRST;
 }
 
 void pizero_spi_write(uint8_t data) {
-    while (!(UCB0IFG & UCTXIFG0)); // Wait for TX buffer to be ready
-    UCB0TXBUF = data; // Transmit data
-    while (UCB0STATW & UCBUSY); // Wait for transmission to complete
+    UCA0TXBUF = data;
 }
 
-uint8_t pizero_spi_read(void) {
-    while (!(UCB0IFG & UCRXIFG0)); // Wait for RX buffer to be ready
-    return UCB0RXBUF; // Return received data
+int pizero_spi_read(void) {
+    int temp = UCA0RXBUF;
+    return temp; // Return received data
 }
-
-void pizero_spi_select(void) {
-    P2OUT &= ~BIT3; // set CS bit low for beginning communication
-}
-
-void pizero_spi_deselect(void) {
-    P2OUT |= BIT3; // Set CS high for ending the communication
-}
-
 
 void cell_I2C_init(void){
+    UCB0CTLW0 |= UCSWRST;
+    UCB0CTLW0 |= UCSSEL__SMCLK | UCMST | UCMSB | UCMODE_3;
+    UCB0BRW = 10;
+    UCB0I2CSA = MAX17048_I2C_address;
+    UCB0CTLW1 |= UCASTP_2; // auto stop mode based on the byte counter
+    UCB0TBCNT = 1; // reading one byte from the slave
+
     P1SEL0 |= BIT6 | BIT7;
     P1SEL1 &= ~(BIT6 | BIT7);
-
-    UCB0CTLW0 |= UCSWRST;
-    UCB0CTLW0 |= UCMST | UCMODE_3 | UCSYNC;
-    UCB0CTLW1 |= UCASTP_2;
-    UCB0BRW = 10; // baudrate = 10
-    UCB0I2CSA = MAX17048_I2C_address;
+    PM5CTL0 &= ~LOCKLPM5;
     UCB0CTLW0 &= ~UCSWRST;
-    UCB0IE |= UCNACKIE;
+
+    UCB0IE |= UCRXIE0 | UCTXIE0;
+    UCB0IFG &= ~(UCRXIFG0 | UCTXIFG0);
 }
 
-float cell_I2C_read_data(){
+float cell_I2C_read_data(void){
 
-    uint16_t data;
-    while(~(UCB0CTLW0 & UCTXSTP));
-    UCB0CTLW0 |= UCTR | UCTXSTT;
-
-    while(~(UCB0IFG & UCTXIFG0));
-    UCB0TXBUF = MAX17048_REG_SOC;
-
-    while(~(UCB0CTL0 & UCTXSTP));
-    UCB0CTLW0 &= ~UCTR;
+    //Transmit register address from which we want to read the state of charge
+    UCB0CTLW0 |= UCTR;
     UCB0CTLW0 |= UCTXSTT;
 
-    while(UCB0CTLW0 & UCTXSTT);
-    while(~(UCB0IFG & UCRXIFG0));
-    data = UCB0RXBUF << 8;
+    while((UCB0IFG & UCSTPIFG) == 0); //waiting for the stp flag to fire to ensure that the max17048 address has been sent and acknowledged by the slave
+    UCB0IFG &= ~UCSTPIFG;
+    //from here it will go to the interrupt service routine
+    //Receive the data from that register containing the state of charge
+    UCB0CTLW0 &= ~UCTR;
+    UCB0CTLW0 |= UCTXSTT;
+    //restarting the communication for receiving the data from the specific register
+    while((UCB0IFG & UCSTPIFG) == 0);
+    UCB0IFG &= ~UCSTPIFG;
 
-    UCB0CTLW0 |= UCTXSTP;
-
-    while(~(UCB0IFG & UCRXIFG0));
-    data |= UCB0RXBUF;
-
-    float result = (data >> 8) + ((data & 0xFF) / 256.0);
+    //converting the result into voltage
+    float result = (read_I2C_data_cell >> 8) + ((read_I2C_data_cell & 0xFF) / 256.0);
 
     return result;
 }
 
-void VC_Sensor_configure_ina3221(void) {
-    VC_Sensor_I2C_write_word(INA3221_REG_CONFIG, CONFIG_VALUE);
-}
-
 void VC_Sensor_I2C_init(void) {
     // Configure pins 3.1 and 3.2 for I2C functionality
+    UCB1CTLW0 |= UCSWRST;
+
+    UCB1CTLW0 |= UCSSEL__SMCLK | UCMST | UCMSB | UCMODE_3;
+    UCB1BRW = 10;
+    UCB1I2CSA = INA3221_I2C_ADDR;
+    UCB1CTLW1 |= UCASTP_2; // auto stop mode based on the byte counter
+    UCB1TBCNT = 1; // reading one byte from the slave
+
     P3SEL0 |= BIT1 | BIT2;
     P3SEL1 &= ~(BIT1 | BIT2);
 
-    // Initialize eUSCI_B0 for I2C master mode
-    UCB0CTLW0 = UCSWRST;                      // Enable SW reset
-    UCB0CTLW0 |= UCMODE_3 | UCMST | UCSYNC;   // I2C master mode, synchronous mode
-    UCB0CTLW1 |= UCASTP_2;                    // Automatic STOP condition
-    UCB0BRW = 10;                             // baudrate = SMCLK / 10
-    UCB0I2CSA = INA3221_I2C_ADDR;             // Slave address
-    UCB0CTLW0 &= ~UCSWRST;                    // Clear SW reset, resume operation
-    UCB0IE |= UCNACKIE;                       // Enable NACK interrupt
+    PM5CTL0 &= ~LOCKLPM5;
+    UCB1CTLW0 &= ~UCSWRST;
+    UCB1IE |= UCRXIE0 | UCTXIE0;
+    UCB1IFG &= ~(UCRXIFG0 | UCTXIFG0);
 }
 
-void VC_Sensor_I2C_write_word(uint8_t reg, uint16_t data) {
-    while (UCB0CTLW0 & UCTXSTP);              // Ensure stop condition got sent
-    UCB0CTLW0 |= UCTR | UCTXSTT;              // I2C TX, start condition
+uint16_t VC_Sensor_I2C_read_word(void) {
 
-    while (!(UCB0IFG & UCTXIFG0));            // Wait for TX buffer to be ready
-    UCB0TXBUF = reg;                          // Send register address
+    //Transmit register address from which we want to read the state of charge
+    UCB1CTLW0 |= UCTR;
+    UCB1CTLW0 |= UCTXSTT;
 
-    while (!(UCB0IFG & UCTXIFG0));            // Wait for TX buffer to be ready
-    UCB0TXBUF = data >> 8;                    // Send MSB
+    while((UCB1IFG & UCSTPIFG) == 0); //waiting for the stp flag to fire to ensure that the max17048 address has been sent and acknowledged by the slave
+    UCB1IFG &= ~UCSTPIFG;
+    //from here it will go to the interrupt service routine
+    //Receive the data from that register containing the state of charge
+    UCB1CTLW0 &= ~UCTR;
+    UCB1TBCNT = 2;
+    UCB1CTLW0 |= UCTXSTT;
 
-    while (!(UCB0IFG & UCTXIFG0));            // Wait for TX buffer to be ready
-    UCB0TXBUF = data & 0xFF;                  // Send LSB
+    //restarting the communication for receiving the data from the specific register
+    while((UCB1IFG & UCSTPIFG) == 0);
+    UCB1IFG &= ~UCSTPIFG;                    // Read LSB
 
-    while (!(UCB0IFG & UCTXIFG0));            // Wait for TX buffer to be ready
-    UCB0CTLW0 |= UCTXSTP;                     // I2C stop condition
-}
+    return read_I2C_data_VC_sensor;
 
-uint16_t VC_Sensor_I2C_read_word(uint8_t reg) {
-
-    uint16_t data;
-
-    while (UCB0CTLW0 & UCTXSTP);              // Ensure stop condition got sent
-    UCB0CTLW0 |= UCTR | UCTXSTT;              // I2C TX, start condition
-
-    while (!(UCB0IFG & UCTXIFG0));            // Wait for TX buffer to be ready
-    UCB0TXBUF = reg;                          // Send register address
-
-    while (!(UCB0IFG & UCTXIFG0));            // Wait for TX buffer to be ready
-    UCB0CTLW0 &= ~UCTR;                       // I2C RX
-    UCB0CTLW0 |= UCTXSTT;                     // I2C repeated start condition
-
-    while (UCB0CTLW0 & UCTXSTT);              // Wait for start condition to be sent
-    while (!(UCB0IFG & UCRXIFG0));            // Wait for RX buffer to be ready
-    data = UCB0RXBUF << 8;                    // Read MSB
-
-    UCB0CTLW0 |= UCTXSTP;                     // I2C stop condition
-    while (!(UCB0IFG & UCRXIFG0));            // Wait for RX buffer to be ready
-    data |= UCB0RXBUF;                        // Read LSB
-
-    return data;
 }
 
 float VC_Sensor_get_shunt_voltage(uint8_t channel) {
-    uint8_t reg;
 
     switch (channel) {
         case 1:
@@ -284,12 +241,11 @@ float VC_Sensor_get_shunt_voltage(uint8_t channel) {
             return -1.0;
     }
 
-    uint16_t raw_value = VC_Sensor_I2C_read_word(reg);
+    uint16_t raw_value = VC_Sensor_I2C_read_word();
     return raw_value * 40e-6;  // Convert raw value to voltage (in volts)
 }
 
 float VC_Sensor_get_bus_voltage(uint8_t channel) {
-    uint8_t reg;
 
     switch (channel) {
         case 1:
@@ -304,8 +260,7 @@ float VC_Sensor_get_bus_voltage(uint8_t channel) {
         default:
             return -1.0;
     }
-
-    uint16_t raw_value = VC_Sensor_I2C_read_word(reg);
+    uint16_t raw_value = VC_Sensor_I2C_read_word();
     return raw_value * 8e-3;  // Convert raw value to voltage (in volts)
 }
 
@@ -337,29 +292,27 @@ void configurePins(void){
 
 }
 
-void SPI_init(void){
-    P2DIR |= LORA_G0 | LORA_RST;
-    P3DIR |= LORA_CS ;
-    P4DIR |= LORA_EN;
-
-    P2OUT &= ~LORA_RST;
-    P3OUT |= LORA_CS;
-    P4OUT &= ~LORA_EN;
-
+void Lora_spi_init(void){
     // configuring the SPI communication ports
-    UCB0CTLW0 |= UCSWRST;      // Put eUSCI state machine in reset
-    UCB0CTLW0 |= UCMST | UCSYNC | UCCKPL | UCMSB; // 3-pin, 8-bit SPI master
-    UCB0CTLW0 |= UCSSEL__SMCLK; // SMCLK
-    UCB0BRW = 0x02;             // /2 clock divider
-    UCB0CTLW0 &= ~UCSWRST;      // Initialize eUSCI state machine
-    UCB0IE |= UCRXIE;
+    UCA1CTLW0 |= UCSWRST;
 
-    P2SEL0 |= LORA_MOSI | LORA_MISO;
-    P3SEL0 |= LORA_SCK;
+    UCA1CTLW0 |= UCSSEL__SMCLK;
+    UCA1BRW   |= 10; //100Khz communication
+    UCA1CTLW0 |= UCMODE_2;
+    UCA1CTLW0 |= UCMSB | UCMST;
+    UCA1CTLW0 |= UCSYNC;
+    UCA1CTLW0 |= UCSTEM;
+
+    UCA1CTLW0 &= ~UCSWRST;
+
+    P3SEL0 |= LORA_MOSI | LORA_MISO | LORA_SCK | LORA_CS;
+    P3SEL1 &= ~(LORA_MOSI | LORA_MISO | LORA_SCK | LORA_CS);
 
 }
 
 void Lora_init(void){
+    P2DIR |= LORA_RST;
+    P2OUT &= ~LORA_RST;
     Lora_reset();
     Lora_enable();
 }
@@ -374,18 +327,13 @@ void Lora_enable(void){
     P4OUT |= LORA_EN;
 }
 
-void SPI_send(uint8_t data){
-    while(!(UCB0IFG & UCTXIFG));
-    UCB0TXBUF = data;
-    while(!(UCB0IFG & UCRXIFG));
-    (void)UCB0RXBUF;
+void Lora_spi_send(uint8_t data){
+    UCA1TXBUF = data;
 }
 
-uint8_t SPI_receive(void){
-    while(!(UCB0IFG & UCRXIFG));
-    UCB0TXBUF = 0x00;   //dummy read t0 generate clock signal
-    while(!(UCB0IFG & UCRXIFG));
-    return UCB0RXBUF;
+int Lora_spi_receive(void){
+    int temp = UCA1RXBUF;
+    return temp;
 }
 
 void configureTimer(void)
@@ -433,6 +381,9 @@ void switch_control(float voltage){
         }
 }
 
+//---------------------------------------------------------------------------------------------------------
+//------------------------------------Interupt Service Routines--------------------------------------------
+
 #pragma vector = TIMER0_A0_VECTOR
 __interrupt void TIMER_A0(void){
     sendResetSignal();
@@ -446,4 +397,21 @@ __interrupt void PORT_2(void){
         P2IFG &= ~WATCHDOG_SIGNAL_PIN;
     }
 }
-
+#pragma vector = USCI_B0_VECTOR
+__interrupt void USCI_B0_ISR(void){
+    if(UCB0IFG & UCTXIFG0){
+        UCB0TXBUF = MAX17048_REG_SOC;
+    }
+    else if (UCB0IFG & UCRXIFG0){
+        read_I2C_data_cell = UCB0RXBUF;
+    }
+}
+#pragma vector = USCI_B1_VECTOR
+__interrupt void USCI_B1_ISR(void){
+    if(UCB1IFG & UCTXIFG0){
+        UCB1TXBUF = reg;
+    }
+    else if (UCB1IFG & UCRXIFG0){
+        read_I2C_data_VC_sensor = UCB1RXBUF;
+    }
+}
